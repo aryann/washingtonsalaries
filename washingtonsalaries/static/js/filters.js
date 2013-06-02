c07b1d294e0df3b7012440b68920d8723f763f9a
Aryan Naraghi <aryan.naraghi@gmail.com>
2013-06-01 18:23:18 -0700

    Redefined the salary filter in terms of the currency filter.

--- washingtonsalaries/static/js/filters.js
+++ washingtonsalaries/static/js/filters.js
@@ -1,13 +1,9 @@
 angular.module("washingtonsalariesFilters", [])
 
-  // Adds a $ sign and commas to the given numeric input.
-  .filter("salary", function() {
+  // Same as the currency filter except the decimal portion is truncated.
+  .filter("salary", function($filter) {
       return function(input) {
-        input = input.toString();
-        var res = [input.substr(0, input.length %3)];
-        for (var i = input.length % 3; i < input.length; i += 3) {
-          res.push(input.substr(i, i + 3))
-        }
-        return "$" + res.join(",");
+        var output = $filter('currency')(input);
+        return output.substr(0, output.length - 3);
       };
     });
