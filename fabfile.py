14c95d8edb6c63f53dc1c049c2581a55b30393bd
Aryan Naraghi <aryan.naraghi@gmail.com>
2013-06-15 16:44:52 -0700

    Made some of the commands more quiet.

--- fabfile.py
+++ fabfile.py
@@ -20,7 +20,7 @@ env.root = os.path.dirname(env.real_fabfile)
 
 
 def prep_jetty_files():
-    local('unzip {root}/lib/jetty* -d {temp_dir}'.format(**env))
+    local('unzip -q -d {temp_dir} {root}/lib/jetty*'.format(**env))
     local('mv {temp_dir}/jetty* {temp_dir}/jetty'.format(**env))
 
     # Disables the Jetty demos.
@@ -34,7 +34,7 @@ def prep_jetty_files():
     local('cp -r {root}/solr {jetty_home}'.format(**env))
 
     # Copies the Solr dependencies into Jetty.
-    local('unzip -d {jetty_home}/webapps/solr {root}/lib/solr*/solr*.war'
+    local('unzip -q -d {jetty_home}/webapps/solr {root}/lib/solr*/solr*.war'
           .format(**env))
     local('cp {root}/lib/solr*/solrj-lib/* {jetty_home}/lib/ext'.format(**env))
 
@@ -62,7 +62,7 @@ def copy_files_to_host():
     put('{temp_dir}/deployment.tar.gz'.format(**env), '/tmp')
 
     with cd('/tmp'):
-        run('tar xzvf deployment.tar.gz')
+        run('tar xzf deployment.tar.gz')
 
         jetty_home_dir = os.path.dirname(env.jetty_home)
         run('mv jetty {0}'.format(jetty_home_dir))
@@ -88,7 +88,7 @@ def prep_deployment(debug=False, keep_jetty_running=True):
     if not keep_jetty_running:
         stop_jetty(local)
     with lcd(env.temp_dir):
-        local('tar zcvf deployment.tar.gz jetty')
+        local('tar zcf deployment.tar.gz jetty')
     print 'Deployment files available at:', env.temp_dir
     if keep_jetty_running:
         print 'Jetty running at: http://localhost:{0}'.format(
