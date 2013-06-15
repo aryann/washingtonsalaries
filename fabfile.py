9ca881116011ca4b298a48d49dff3dfc51df0dae
Aryan Naraghi <aryan.naraghi@gmail.com>
2013-06-15 13:06:52 -0700

    Adds the depencencies to the repo. This is not an ideal practice, but the benefits of having a hermetic project outweigh the costs of adding binaries to the repo.

--- fabfile.py
+++ fabfile.py
@@ -12,29 +12,33 @@ env.root = os.path.dirname(env.real_fabfile)
 
 def prep_jetty():
     env.temp_dir = tempfile.mkdtemp()
-    for dep in ['jetty', 'solr']:
-        local('unzip {root}/dist/{dep}* -d {temp_dir}'.format(dep=dep, **env))
-        local('mv {temp_dir}/{dep}* {temp_dir}/{dep}'.format(dep=dep, **env))
-        env[dep + '_dir'] = os.path.join(env.temp_dir, dep)
 
-    # Disables the demos.
+    local('unzip {root}/lib/jetty* -d {temp_dir}'.format(**env))
+    local('mv {temp_dir}/jetty* {temp_dir}/jetty'.format(**env))
+    env.jetty_dir = os.path.join(env.temp_dir, 'jetty')
+
+    # Disables the Jetty demos.
     local('rm {jetty_dir}/start.d/900-demo.ini'.format(**env))
 
+    # Copies the static files to Jetty's webapps/.
     local('rm -rf {jetty_dir}/webapps/ROOT'.format(**env))
     local('cp -r {root}/client {jetty_dir}/webapps/ROOT'.format(**env))
 
-    # Copies the Solr dir to the Jetty dir.
+    # Copies the Solr home dir to the Jetty dir.
     local('cp -r {root}/solr {jetty_dir}'.format(**env))
 
-    local('mv {solr_dir}/dist/solrj-lib/* {jetty_dir}/lib/ext'.format(
-            **env))
-    local('mv {solr_dir}/dist/solr*.war {jetty_dir}/webapps/solr.war'.format(
-            **env))
+    # Copies the Solr dependencies into Jetty.
+    local('cp lib/solr*/solr*.war {jetty_dir}/webapps/solr.war'.format(**env))
+    local('cp lib/solr*/solrj-lib/* {jetty_dir}/lib/ext'.format(**env))
 
 
-def populate_solr(port=8080):
-    local('{root}/data/add_to_solr {root}/data/data.csv {server_port}'.format(
-            server_port=port, **env))
+def populate_solr(port=8080, debug=False):
+    local("""
+        {root}/data/add_to_solr {root}/data/data.csv {server_port} \
+                                                     {lines_to_process}
+        """.format(server_port=port,
+                   lines_to_process=100 if debug else '',
+                   **env))
 
 
 def start_jetty(jetty_home=None):
@@ -50,7 +54,7 @@ def start_jetty(jetty_home=None):
         """.format(jetty_home=jetty_home))
 
 
-def full_deploy():
+def full_deploy(debug=False):
     prep_jetty()
     start_jetty()
-    populate_solr()
+    populate_solr(debug=debug)
